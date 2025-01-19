881efc378ddd56c478600fd6f46f3b657c4de38e
slowmarc <markos.yiakoumis@icloud.com>
2025-01-19 21:55:57 +0000

    Update config.lua

--- shared/config.lua
+++ shared/config.lua
@@ -1,11 +1,11 @@
 BondrixClock.Config = {}
 
 -- The time zone to sync the time and date to.
--- Currently only 'America/Los_Angeles' is supported.
+-- Currently, only 'America/Los_Angeles' is supported.
 BondrixClock.Config.timeZone = 'America/Los_Angeles'
 
 -- Should the server attempt to sync the time and date after initializing?
 BondrixClock.Config.sync = true
 -- How often should the server attempt to sync the time to all clients (in milliseconds)?
--- Default is 1000 ms, meaning every seconds. You should keep this value relatively low.
+-- Default is 1000 ms, meaning every second. You should keep this value relatively low.
 BondrixClock.Config.syncInterval = 1000
